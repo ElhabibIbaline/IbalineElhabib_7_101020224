58e502af4c19a29fa64ce4b7bc8cc7187ab5afbe
elhabib ibaline <h.ibaline@gmail.com>
2024-10-23 18:57:29 +0200

    ajout react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/logement",
+    element: <h1>Logements</h1>,
+  },
+  {
+    path: "/about",
+    element: <h1>A propos</h1>,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
